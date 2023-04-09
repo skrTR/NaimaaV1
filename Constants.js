a231ff05af84230bcdd60c4783b989c201549979
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-04-10 01:44:51 +0800

    a

--- Constants.js
+++ Constants.js
@@ -1,4 +1,5 @@
 // export const api = "http://159.223.82.71";
 // export const api = "http://128.199.94.192";
-export const api = "https://naimaaserver.com";
+// export const api = "https://naimaaserver.com";
+export const api = "http://206.189.46.13";
 // export const api = "http://192.168.1.90:8000";
